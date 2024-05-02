42f56d954dd9ba3071cf50b81c0c9ce18627eb16
Esther White <monacodelisa@gmail.com>
2024-05-02 14:19:50 +0300

    feature(settings-dialog): input validations and error handeling

--- angular-primeng-app/src/app/partials/settings-dialog/settings-dialog.component.ts
+++ angular-primeng-app/src/app/partials/settings-dialog/settings-dialog.component.ts
@@ -22,8 +22,12 @@ import { ButtonModule } from "primeng/button";
 export class SettingsDialogComponent implements OnInit {
 	visible = false;
 	blogURL: string = "hashnode.anguhashblog.com";
+  newBlogInput: string = "";
 	newBlogURL: string = "";
 	blogURLChanged: boolean = false;
+	noBlogFound: boolean = false;
+	emptyInput: boolean = false;
+	invalidInput: boolean = false;
 	blogService: BlogService = inject(BlogService);
 
 	ngOnInit() {
@@ -35,21 +39,55 @@ export class SettingsDialogComponent implements OnInit {
 		}
 	}
 
-  changeBlogURL(): void {
-    this.blogService.setBlogURL(this.newBlogURL);
-    this.blogURL = this.blogService.getBlogURL();
-    if (this.blogURL === "hashnode.anguhashblog.com") {
+	changeBlogURL(): void {
+		this.noBlogFound = false;
+		if (this.newBlogInput === "") {
+			this.emptyInput = true;
+			return;
+		} else if ( this.newBlogInput !== "") {
+      this.emptyInput = false;
+
+      if (this.newBlogInput.includes("https://") || this.newBlogInput.endsWith("/")) {
+        const cleanedBlogURL = this.newBlogInput.split("https://")[1];
+        this.newBlogURL = cleanedBlogURL.split("/")[0];
+
+      } else {
+        this.newBlogURL = this.newBlogInput;
+      }
+
+			this.blogService.getBlogInfo(this.newBlogURL).subscribe((blogInfo) => {
+				if (blogInfo === null) {
+					this.noBlogFound = true;
+					this.blogURLChanged = false;
+					this.newBlogInput = "";
+				} else {
+					this.blogService.setBlogURL(this.newBlogURL);
+					this.blogURL = this.blogService.getBlogURL();
+          this.blogURLChanged = true;
+          this.visible = false;
+		      window.location.reload();
+				}
+			});
+		} else if (this.blogURL === "hashnode.anguhashblog.com") {
       this.blogURLChanged = false;
     } else {
-      this.blogURLChanged = true;
-    }
-  }
+			this.noBlogFound = true;
+			this.emptyInput = false;
+			this.blogURLChanged = false;
+      this.invalidInput = true;
+			this.newBlogInput = "";
+		}
+	}
 
-  resetBlogURL(): void {
-    this.blogService.resetBlogURL();
-    this.blogURL = this.blogService.getBlogURL();
-    this.blogURLChanged = false;
-  }
+
+	resetBlogURL(): void {
+		this.blogService.resetBlogURL();
+		this.blogURL = this.blogService.getBlogURL();
+		this.emptyInput = false;
+		this.blogURLChanged = false;
+		this.visible = false;
+		window.location.reload();
+	}
 
 	showDialog() {
 		this.visible = true;
