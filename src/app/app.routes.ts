8fe3ccaf31de339437bb7a28903ba675be38e56a
Esther White <monacodelisa@gmail.com>
2024-06-25 14:09:08 +0300

    fix(sidebar layout): going back to using layout component to avoid issues with primeng sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,30 @@
-import { Routes } from '@angular/router';
-import { PostDetailsComponent } from './components/post-details/post-details.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { SeriesComponent } from './components/series/series.component';
+import { Routes } from "@angular/router";
+import { PostDetailsComponent } from "./components/post-details/post-details.component";
+import { PostsComponent } from "./components/posts/posts.component";
+import { SeriesComponent } from "./components/series/series.component";
+import { LayoutComponent } from "./components/layout/layout.component";
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: PostsComponent
-  },
-  {
-    path: 'series/:slug',
-    component: SeriesComponent
-  },
-  {
-    path: 'post/:postSlug',
-    component: PostDetailsComponent
-  },
-  {
-    path: '**',
-    redirectTo: ''
-  }
+	{
+		path: "",
+		component: LayoutComponent,
+		children: [
+			{
+				path: "",
+				component: PostsComponent,
+			},
+      {
+        path: "series/:slug",
+        component: SeriesComponent,
+      },
+		],
+	},
+	{
+		path: "post/:postSlug",
+		component: PostDetailsComponent,
+	},
+	{
+		path: "**",
+		redirectTo: "",
+	},
 ];
